9cf37d0ddb90b300e8ae1b09166b81182f4c1ffc
Glenn Watson <GitHub@glennwatson.net>
2019-06-14 21:11:15 +1000

    Change back to a private member

--- src/Pharmacist.Core/Generation/Generators/EventGeneratorBase.cs
+++ src/Pharmacist.Core/Generation/Generators/EventGeneratorBase.cs
@@ -67,7 +67,7 @@ namespace Pharmacist.Core.Generation.Generators
                 .WithLeadingTrivia(XmlSyntaxFactory.GenerateSummarySeeAlsoComment("Gets an observable which signals when the {0} event triggers.", eventDetails.FullName));
         }
 
-        protected static (ArrowExpressionClauseSyntax, TypeSyntax) GenerateFromEventExpression(IEvent eventDetails, IMethod invokeMethod, string dataObjectName)
+        private static (ArrowExpressionClauseSyntax, TypeSyntax) GenerateFromEventExpression(IEvent eventDetails, IMethod invokeMethod, string dataObjectName)
         {
             var returnType = SyntaxFactory.IdentifierName(eventDetails.ReturnType.GenerateFullGenericName());
 
